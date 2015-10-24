0867f76b7598e1a7e64602753efae009e5d1a8a8
Sam A. Horvath-Hunt <samhh.web@gmail.com>
2015-10-24 13:12:36 +0100

    Updated gulpfile - removed uglify, added babel

--- gulpfile.js
+++ gulpfile.js
@@ -18,29 +18,19 @@ var filter = require('gulp-filter');
 /*
 * (S)CSS
 */
-// libsass doesn't work with /deep/ in css, and the new syntax for /deep/, >>>, has yet to be implemented, see gulp-sass GitHub issue - makes polymer styling impossible
-// if /deep/ support is required, revert to using gulp-ruby-sass
-// gulp-sass (libsass) reduces the process time from >600ms down to <6ms versus gulp-ruby-sass
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var prefix = require('gulp-autoprefixer');
+var pixrem = require('gulp-pixrem');
 var postcss = require('gulp-postcss');
 var lost = require('lost');
 
 /*
 * JS
 */
+var babel = require('gulp-babel');
 var jshint = require('gulp-jshint');
-var minifyjs = require('gulp-uglify');
 var concat = require('gulp-concat');
-
-/*
-* TO BE ADDED
-*/
-// will incorporate this once csscomb is more compatible with sass/scss, see: https://github.com/csscomb/csscomb.js/issues/360
-// var csscomb = require('gulp-csscomb');
-
-
 
 /*
 * VARS
@@ -72,21 +62,15 @@ gulp.task('default', ['browser-sync', 'watch']);
 
 gulp.task('browser-sync', ['sass-process-minify'], function() {
 	browserSync({
-		proxy: 'localhost:4567',
-		files: ['css/style.css', 'js/main.js', '*.php']
+		proxy: 'localhost:4568',
+		files: ['css/style.css', 'js/main.js', '*.php', 'templates/*']
 	});
 });
 
 gulp.task('watch', function () {
-	// gulp.watch('css/src/*.scss', ['sass-csscomb']);
 	gulp.watch('css/src/*.scss', ['sass-process-minify']);
 	gulp.watch('js/src/*.js', ['js-minify'])
 });
-
-// gulp.task('sass-csscomb', function() {
-// 	return gulp.src('css/src/*.scss')
-// 		.pipe(csscomb())
-// });
 
 gulp.task('sass-process-minify', function() {
 	// not being returned intentionally, see: https://github.com/dlmanning/gulp-sass/wiki/Common-Issues-and-Their-Fixes#gulp-watch-stops-working-on-an-error
@@ -96,11 +80,11 @@ gulp.task('sass-process-minify', function() {
 		.pipe(sass({
 			outputStyle: 'compressed'
 		}).on('error', sass.logError))
-		// .pipe(sass().on('error', sass.logError))
 		.pipe(postcss([
 			lost()
 		]))
 		.pipe(prefix(['last 10 versions', '> 5%', 'ie >= 8']))
+		.pipe(pixrem())
 		.pipe(sourcemaps.write('./', {
 			includeContent: false
 		}))
@@ -113,13 +97,13 @@ gulp.task('sass-process-minify', function() {
 
 gulp.task('js-minify', function() {
 	return gulp.src('js/src/*.js')
-		// plumber/notify not being triggered
+		// notify not being triggered
 		.pipe(plumber(plumberOptions))
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'))
 		.pipe(sourcemaps.init())
 		.pipe(concat('main.js'))
-		.pipe(minifyjs())
+		.pipe(babel())
 		.pipe(sourcemaps.write('./', {
 			includeContent: false
 		}))
