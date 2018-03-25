fd8796a55a6ed19315b413c741488afd227aa845
Sam A. Horvath-Hunt <samhh.web@gmail.com>
2018-03-25 16:00:57 +0100

    Temporarily disable AUR update check in script

--- linux/home/scripts/updates.sh
+++ linux/home/scripts/updates.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 updates_arch=$(checkupdates | wc -l)
-updates_aur=$(pacaur -k | wc -l)
+#updates_aur=$(pacaur -k | wc -l)
+updates_aur=0
 
 echo $(( "$updates_arch" + "$updates_aur" ))
 
