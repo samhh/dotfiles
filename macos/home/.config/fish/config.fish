56db67d3b72152880ee4ea0cc1091c7b519d89d3
Sam A. Horvath-Hunt <git@samhh.com>
2018-10-10 13:31:26 +0100

    Add Fish config for macos

---
+++ macos/home/.config/fish/config.fish
@@ -0,0 +1,31 @@
+# Fish configuration
+fish_vi_key_bindings
+set fish_greeting
+
+# Exported variables
+set -x VISUAL nvim
+set -x EDITOR $VISUAL
+
+# Generic aliases
+alias c='clear'
+alias cdc='cd; and clear'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias rmds='find . -name "*.DS_Store" -type f -delete'
+
+# Command drop-in replacement aliases
+alias grep='rg -uu'
+alias rm='trash'
+
+# App-specific (excl/ package management) aliases
+alias npmlsg='npm ls -g --depth=0'
+
+# Arch package management-specific aliases
+alias up='brew update; and brew upgrade'
+alias clear_cache='brew cu; and brew cleanup'
+
+# Use shell environment in sudo so that sudo knows the above aliases
+alias sudo='sudo -sE'
+
