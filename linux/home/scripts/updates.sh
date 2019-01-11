10c8b170cb8dbb9b7ba4fee7e2f8ae63e73e80b9
Sam A. Horvath-Hunt <git@samhh.com>
2019-01-11 22:35:43 +0000

    Do not print arch update check updates; add check to updates script in Polybar

--- linux/home/scripts/updates.sh
+++ linux/home/scripts/updates.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-updates_arch=$(checkupdates | wc -l)
+updates_arch=$(checkupdates 2> /dev/null | wc -l)
 updates_aur=$(checkupdates-aur | wc -l)
 
 echo $(( "$updates_arch" + "$updates_aur" ))
