9042971228de0b1535e5e2080ee59242268936e8
Sam A. Horvath-Hunt <git@samhh.com>
2019-02-23 14:32:19 +0000

    Some bar updates

--- linux/home/scripts/updates.sh
+++ linux/home/scripts/updates.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 updates_arch=$(checkupdates 2> /dev/null | wc -l)
-updates_aur=$(checkupdates-aur | wc -l)
+updates_aur=$(yay -Qum 2> /dev/null | wc -l)
 
 echo $(( "$updates_arch" + "$updates_aur" ))
 
