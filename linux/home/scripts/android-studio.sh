93727832b8dceb8cd672d26a9fecc4a119eac1f4
Sam A. Horvath-Hunt <git@samhh.com>
2020-04-18 18:59:01 +0100

    Remove unused scripts

--- linux/home/scripts/android-studio.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# See: https://github.com/swaywm/sway/wiki#issues-with-java-applications
-_JAVA_AWT_WM_NONREPARENTING=1 android-studio &
-
--- linux/home/scripts/ping.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ping 8.8.8.8 -c 1 | awk -F" |=" '/time=/{printf "%i%s\n", $10, "ms"}'
-
