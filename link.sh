d38ae945ccd950298c0d7d33b68a896c2f134fa1
Sam A. Horvath-Hunt <git@samhh.com>
2020-07-25 18:14:27 +0100

    Update link script for branching approach

--- link.sh
+++ link.sh
@@ -3,15 +3,5 @@
 # Set PWD to location of script (i.e. dotfiles repo root)
 cd "$(dirname "$0")"
 
-# Establish which OS we're working on
-if [ `uname` == 'Linux' ]; then
-  osdir="linux"
-elif [ `uname` == 'Darwin' ]; then
-  osdir="macos"
-else
-  exit 1
-fi
-
-stow -R --adopt home -d $osdir -t $HOME
-stow -R --adopt home -d shared -t $HOME
+stow -R --adopt home -t $HOME
 
