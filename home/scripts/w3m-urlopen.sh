76c46460668ea6efd869214c1e817bb056608d95
Sam A. Horvath-Hunt <hello@samhh.com>
2020-09-06 14:16:56 +0100

    Fix aerc/w3m urlopen script blocking process

--- home/scripts/w3m-urlopen.sh
+++ home/scripts/w3m-urlopen.sh
@@ -4,5 +4,5 @@ export SOCKS_SERVER="127.0.0.1:1"
 
 content=$(socksify w3m -T text/html -cols $(tput cols) -dump -o display_image=false -o display_link_number=true)
 url=$(echo "$content" | sed -n '/References:/,$ p' | tail -n +3 | cut -d ' ' -f 2 | sed -n "$1 p")
-exec xdg-open $url
+exec xdg-open $url & disown
 
