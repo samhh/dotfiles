11ddc9aab40a27ea8ab126aa96930f069b4ff4b9
Sam A. Horvath-Hunt <hello@samhh.com>
2020-10-06 11:31:48 +0100

    Update backup script for updated docs dir structure

--- home/scripts/backup.sh
+++ home/scripts/backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-pacman -Qet > ~/docs/personal/logs/packages.txt
+pacman -Qet > ~/docs/logs/packages.txt
+tree /mnt/nas/tv/ > ~/docs/logs/tv.txt
+tree /mnt/nas/movies/ > ~/docs/logs/movies.txt
 borgmatic
 backblaze-b2 sync --delete --replaceNewer ~/.docs_repo/ b2://docs-borg
 
