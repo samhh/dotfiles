9a9ee23e1c0dd4d4f992b9a0621bf71b0a68bf82
Sam A. Horvath-Hunt <hello@samhh.com>
2020-11-09 16:06:46 +0000

    Fix web search script failing with hyphen head char

--- home/scripts/web-search.sh
+++ home/scripts/web-search.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-query=$(echo | rofi -dmenu -l 0 -p web)
+query=$(rofi -dmenu -l 0 -p web)
 [[ -n $query ]] || exit
 
 banged=$(bangin "$query" || echo "$query")
-vimb "$banged"
+vimb -- "$banged"
 
