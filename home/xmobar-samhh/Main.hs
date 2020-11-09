83b58fcd7aa1cae6c9e60c9a4bde0eb64d6680b6
Sam A. Horvath-Hunt <hello@samhh.com>
2020-11-09 16:37:02 +0000

    xmobar Main formatting

--- home/xmobar-samhh/Main.hs
+++ home/xmobar-samhh/Main.hs
@@ -12,25 +12,26 @@ cfg =
       bgColor = "#2e3440",
       fgColor = "#d8dee9",
       position = Top,
-      sepChar = "%", -- delineator between plugin names and straight text
-      alignSep = "}{", -- separator between left-right alignment
+      sepChar = "%",
+      alignSep = "}{",
       template = " %StdinReader% | %cpu% | %cputemps% | %memory% | %dynnetwork% }{ %mpris2%%mpd% | %alsa:default:Master% | %email% unread | %date% ",
-      lowerOnStart = True, -- send to bottom of window stack on start
-      hideOnStart = False, -- start with window unmapped (hidden)
-      allDesktops = True, -- show on all desktops
-      overrideRedirect = True, -- set the Override Redirect flag (Xlib)
-      pickBroadest = False, -- choose widest display (multi-monitor)
-      persistent = True, -- enable/disable hiding (True = disabled)
+      lowerOnStart = True,
+      hideOnStart = False,
+      allDesktops = True,
+      overrideRedirect = True,
+      pickBroadest = False,
+      persistent = True,
       commands =
-        [ Run $ StdinReader,
+        [ Run StdinReader,
+
           Run $
             DynNetwork
               [ "--template",
                 "<dev>: <tx> kB/s up, <rx> kB/s down",
                 "--Low",
-                "50000", -- units: B/s
+                "50000",
                 "--High",
-                "1000000", -- units: B/s
+                "1000000",
                 "--low",
                 "#d8dee9",
                 "--normal",
@@ -39,14 +40,15 @@ cfg =
                 "#ebcb8b"
               ]
               10,
+
           Run $
             Cpu
               [ "--template",
                 "cpu: <total>%",
                 "--Low",
-                "50", -- units: %
+                "50",
                 "--High",
-                "85", -- units: %
+                "85",
                 "--low",
                 "#d8dee9",
                 "--normal",
@@ -55,13 +57,6 @@ cfg =
                 "#ebcb8b"
               ]
               10,
-          -- , Run $ CoreTemp       [ "--template" , "temp: <core0>°C|<core1>°C"
-          --                      , "--Low"      , "40"
-          --                      , "--High"     , "75"
-          --                      , "--low"      , "#d8dee9"
-          --                      , "--normal"   , "#d8dee9"
-          --                      , "--high"     , "#ebcb8b"
-          --                      ] 50
 
           Run $ CommandReader "~/scripts/poll-script.sh ~/scripts/cpu-temps.sh 2s" "cputemps",
 
@@ -83,6 +78,7 @@ cfg =
                 "#ebcb8b"
               ]
               10,
+
           Run $
             Alsa
               "default"
@@ -95,6 +91,7 @@ cfg =
                 "--offc",
                 "#ebcb8b"
               ],
+
           Run $
             Alsa
               "pcm.usb"
@@ -107,6 +104,7 @@ cfg =
                 "--offc",
                 "#d8dee9"
               ],
+
           Run $
             Mpris2
               "mpv"
@@ -116,12 +114,14 @@ cfg =
                 ""
               ]
               10,
+
           Run $
             MPD
               [ "--template",
                 "<fc=#81a1c1><state></fc>: <artist> - <title> (<volume>%)"
               ]
               10,
+
           Run $ Date "<fc=#e5e9f0>%H:%M, %d/%m</fc>" "date" 10
         ]
     }
