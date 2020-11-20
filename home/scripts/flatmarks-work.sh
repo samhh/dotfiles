273c5ccb3f23c9f838dc3209b4127fbdc3d9de66
Sam A. Horvath-Hunt <hello@samhh.com>
2020-11-20 13:52:40 +0000

    Add second, distinct flatmarks script for work

---
+++ home/scripts/flatmarks-work.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+base=~/.bookmarks-work/
+
+# Find everything in bookmarks dir not a directory and not matching git pattern
+readarray -d '' paths < <(find $base -type f -not -path '*/.git*' -print0)
+
+# Remove prefix from strings
+paths=( "${paths[@]#"$base"}" )
+
+selected=$(printf '%s\n' "${paths[@]}" | rofi -window-title bookmark-work -dmenu)
+
+[[ -n $selected ]] || exit
+
+file="$base$selected"
+
+url=$(head -1 $file)
+
+vimb -p work $url
+
