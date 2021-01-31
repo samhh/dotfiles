7a7b3dce50d205fe2fe4d31651ab3f621235d4cb
Sam A. Horvath-Hunt <hello@samhh.com>
2021-01-31 12:49:23 +0000

    Format xmobar config manually

--- home/xmobar-samhh/Main.hs
+++ home/xmobar-samhh/Main.hs
@@ -16,98 +16,82 @@ cfg =
       alignSep = "}{",
       template = " %StdinReader% | %cpu% | %cputemps% | %memory% | docker: %docker% | %wlo1% }{ %mpris2%%mpd% | %default:Master% | rss: %rss% | email: %email% | %date% ",
       commands =
-        [ Run StdinReader,
-
-          Run $
-            Network "wlo1"
-              [ "--template",
-                "net: <tx> kB/s up, <rx> kB/s down",
-                "--Low",
-                "50000",
-                "--High",
-                "1000000",
-                "--low",
-                "#d8dee9",
-                "--normal",
-                "#d8dee9",
-                "--high",
-                "#ebcb8b"
-              ]
-              10,
-
-          Run $
-            Cpu
-              [ "--template",
-                "cpu: <total>%",
-                "--Low",
-                "50",
-                "--High",
-                "85",
-                "--low",
-                "#d8dee9",
-                "--normal",
-                "#d8dee9",
-                "--high",
-                "#ebcb8b"
-              ]
-              10,
-
-          Run $ CommandReader "~/scripts/poll-script.sh ~/scripts/cpu-temps.sh 2s" "cputemps",
-
-          Run $ CommandReader "~/scripts/poll-script.sh ~/scripts/rss-unread.sh 2h" "rss",
-
-          Run $ CommandReader "~/scripts/poll-script.sh ~/scripts/email-unread.sh 15m" "email",
-
-          Run $
-            Memory
-              [ "--template",
-                "mem: <usedratio>%",
-                "--Low",
-                "10",
-                "--High",
-                "75",
-                "--low",
-                "#d8dee9",
-                "--normal",
-                "#d8dee9",
-                "--high",
-                "#ebcb8b"
-              ]
-              10,
-
-          Run $ CommandReader "~/scripts/poll-script.sh ~/scripts/docker-status.sh 1m" "docker",
-
-          Run $
-            Volume
-              "default"
-              "Master"
-              [ "--template",
-                "Out: <volume>% <status>",
-                "--",
-                "-C",
-                "#d8dee9",
-                "-c",
-                "#ebcb8b"
-              ]
-              10,
-
-          Run $
-            Mpris2
-              "mpv"
-              [ "--template",
-                "<title> / ",
-                "--nastring",
-                ""
-              ]
-              10,
-
-          Run $
-            MPD
-              [ "--template",
-                "<fc=#81a1c1><state></fc>: <artist> - <title> (<volume>%)"
-              ]
-              10,
-
-          Run $ Date "<fc=#e5e9f0>%H:%M, %d/%m</fc>" "date" 10
+        [ Run StdinReader
+        , Run $ CommandReader "~/scripts/poll-script.sh ~/scripts/cpu-temps.sh 2s" "cputemps"
+        , Run $ CommandReader "~/scripts/poll-script.sh ~/scripts/rss-unread.sh 2h" "rss"
+        , Run $ CommandReader "~/scripts/poll-script.sh ~/scripts/email-unread.sh 15m" "email"
+        , Run $ CommandReader "~/scripts/poll-script.sh ~/scripts/docker-status.sh 1m" "docker"
+        , Run $ Network
+            "wlo1"
+            [ "--template"
+            , "net: <tx> kB/s up, <rx> kB/s down"
+            , "--Low"
+            , "50000"
+            , "--High"
+            , "1000000"
+            , "--low"
+            , "#d8dee9"
+            , "--normal"
+            , "#d8dee9"
+            , "--high"
+            , "#ebcb8b"
+            ]
+            10
+        , Run $ Cpu
+            [ "--template"
+            , "cpu: <total>%"
+            , "--Low"
+            , "50"
+            , "--High"
+            , "85"
+            , "--low"
+            , "#d8dee9"
+            , "--normal"
+            , "#d8dee9"
+            , "--high"
+            , "#ebcb8b"
+            ]
+            10
+        , Run $ Memory
+            [ "--template"
+            , "mem: <usedratio>%"
+            , "--Low"
+            , "10"
+            , "--High"
+            , "75"
+            , "--low"
+            , "#d8dee9"
+            , "--normal"
+            , "#d8dee9"
+            , "--high"
+            , "#ebcb8b"
+            ]
+            10
+        , Run $ Volume
+            "default"
+            "Master"
+            [ "--template"
+            , "Out: <volume>% <status>"
+            , "--"
+            , "-C"
+            , "#d8dee9"
+            , "-c"
+            , "#ebcb8b"
+            ]
+            10
+        , Run $ Mpris2
+            "mpv"
+            [ "--template"
+            , "<title> / "
+            , "--nastring"
+            , ""
+            ]
+            10
+        , Run $ MPD
+            [ "--template"
+            , "<fc=#81a1c1><state></fc>: <artist> - <title> (<volume>%)"
+            ]
+            10
+        , Run $ Date "<fc=#e5e9f0>%H:%M, %d/%m</fc>" "date" 10
         ]
     }
