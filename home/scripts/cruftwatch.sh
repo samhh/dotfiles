4bcf60af9ad13643e382d304cbf794da52561862
Sam A. Horvath-Hunt <hello@samhh.com>
2021-03-06 13:21:49 +0000

    Add more cruftwatch excludes

--- home/scripts/cruftwatch.sh
+++ home/scripts/cruftwatch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-inotifywait -m ~/ -e create --exclude "(\.steampid)|(\.steampath)" |
+inotifywait -m ~/ -e create --exclude "(\.steampid)|(\.steampath)|(\.Xauthority-c)|(\.Xauthority-l)|(\.b2_account_info-journal)" |
   while read dir action file; do
     notify-send "File created" "$dir$file"
   done
