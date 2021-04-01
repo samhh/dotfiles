0fdd4864cbc2e5f68930d363c0a286866b437548
Sam A. Horvath-Hunt <hello@samhh.com>
2021-04-01 12:51:53 +0100

    Don't adopt target directories with Stow
    It causes a bunch of strange problems (duplicate root-owned dirs showing
    up in the repo, breaking linking). There's also a massive warning label
    on it in the manpage.

--- link.sh
+++ link.sh
@@ -3,6 +3,6 @@
 # Set PWD to location of script (i.e. dotfiles repo root)
 cd "$(dirname "$0")"
 
-stow -R --adopt root -t /
-stow -R --adopt home -t $HOME
+stow -R root -t /
+stow -R home -t $HOME
 
