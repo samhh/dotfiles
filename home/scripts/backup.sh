51c76a00445dfaba773f5d04a8215ccea8df95aa
Sam A. Horvath-Hunt <hello@samhh.com>
2021-04-02 22:14:59 +0100

    Move rarely-accessed backups to NAS

--- home/scripts/backup.sh
+++ home/scripts/backup.sh
@@ -3,15 +3,15 @@
 keyId=$(pass misc/backblaze.com/app-key-id-pass)
 appKey=$(pass misc/backblaze.com/app-key-pass)
 
-mkdir -p ~/logs/
-pacman -Qet > ~/logs/packages.txt
-tree /mnt/nas/tv/ > ~/logs/tv.txt
-tree /mnt/nas/movies/ > ~/logs/movies.txt
+mkdir -p /mnt/nas/logs/
+pacman -Qet > /mnt/nas/logs/packages.txt
+tree /mnt/nas/tv/ > /mnt/nas/logs/tv.txt
+tree /mnt/nas/movies/ > /mnt/nas/logs/movies.txt
 
 offlineimap
 
-duplicity --encrypt-key hello@samhh.com ~/keys/ "b2://$keyId:$appKey@keys-dup"
-duplicity --encrypt-key hello@samhh.com ~/logs/ "b2://$keyId:$appKey@logs-dup"
-duplicity --encrypt-key hello@samhh.com ~/mail/ "b2://$keyId:$appKey@mail-dup"
+duplicity --encrypt-key hello@samhh.com /mnt/nas/keys/ "b2://$keyId:$appKey@keys-dup"
+duplicity --encrypt-key hello@samhh.com /mnt/nas/logs/ "b2://$keyId:$appKey@logs-dup"
+duplicity --encrypt-key hello@samhh.com /mnt/nas/mail/ "b2://$keyId:$appKey@mail-dup"
 duplicity --encrypt-key hello@samhh.com ~/vault/ "b2://$keyId:$appKey@vault-dup"
 
