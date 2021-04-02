a37fa009347253dcd647388c416a5092418190da
Sam A. Horvath-Hunt <hello@samhh.com>
2021-04-02 16:41:08 +0100

    Add keys backup

--- home/scripts/backup.sh
+++ home/scripts/backup.sh
@@ -8,6 +8,7 @@ pacman -Qet > ~/logs/packages.txt
 tree /mnt/nas/tv/ > ~/logs/tv.txt
 tree /mnt/nas/movies/ > ~/logs/movies.txt
 
+duplicity --encrypt-key hello@samhh.com ~/keys/ "b2://$keyId:$appKey@keys-dup"
 duplicity --encrypt-key hello@samhh.com ~/logs/ "b2://$keyId:$appKey@logs-dup"
 duplicity --encrypt-key hello@samhh.com ~/vault/ "b2://$keyId:$appKey@vault-dup"
 
