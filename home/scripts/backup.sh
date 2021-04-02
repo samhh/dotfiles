04a39ae38e756e32da8227a73b7ce650705eacac
Sam A. Horvath-Hunt <hello@samhh.com>
2021-04-02 14:57:39 +0100

    Migrate from borg to duplicity

--- home/scripts/backup.sh
+++ home/scripts/backup.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-pacman -Qet > ~/docs/logs/packages.txt
-tree /mnt/nas/tv/ > ~/docs/logs/tv.txt
-tree /mnt/nas/movies/ > ~/docs/logs/movies.txt
-borgmatic
-backblaze-b2 sync --delete --replaceNewer ~/.docs_repo/ b2://docs-borg
+keyId=$(pass misc/backblaze.com/app-key-id-pass)
+appKey=$(pass misc/backblaze.com/app-key-pass)
+
+mkdir -p ~/logs/
+pacman -Qet > ~/logs/packages.txt
+tree /mnt/nas/tv/ > ~/logs/tv.txt
+tree /mnt/nas/movies/ > ~/logs/movies.txt
+
+duplicity --encrypt-key hello@samhh.com ~/logs/ "b2://$keyId:$appKey@logs-dup"
+duplicity --encrypt-key hello@samhh.com ~/vault/ "b2://$keyId:$appKey@vault-dup"
 
