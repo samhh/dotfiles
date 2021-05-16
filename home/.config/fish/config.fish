c2cc8d3f5a4c755416de581b4fb34ba304426f70
Sam A. Horvath-Hunt <hello@samhh.com>
2021-05-16 21:40:19 +0100

    Improve fish config comments

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,6 +1,8 @@
-# Fish configuration
-fish_vi_key_bindings
+# Disable greeting
 set fish_greeting
+
+# vi-like keybinds
+fish_vi_key_bindings
 
 # Exported variables
 ## Explicitly define XDG defaults that are used below
