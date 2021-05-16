c65757e1bc8e1581f330c53e4244358c67fe8b3e
Sam A. Horvath-Hunt <hello@samhh.com>
2021-05-16 21:59:44 +0100

    Ignore eslint_d dotfile
    Hopefully this supports XDG soon.

--- home/scripts/cruftwatch.sh
+++ home/scripts/cruftwatch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-inotifywait -m ~/ -e create --exclude "(\.steampid)|(\.steampath)|(\.Xauthority-c)|(\.Xauthority-l)|(\.b2_account_info-journal)" |
+inotifywait -m ~/ -e create --exclude "(\.steampid)|(\.steampath)|(\.Xauthority-c)|(\.Xauthority-l)|(\.b2_account_info-journal)|(\.eslint_d)" |
   while read dir action file; do
     notify-send "File created" "$dir$file"
   done
