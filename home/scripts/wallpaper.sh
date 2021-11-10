82c2c8e2c6b1e3103f70277ac5c742d0c221bd84
Sam A. Horvath-Hunt <hello@samhh.com>
2021-11-10 15:55:25 +0000

    Support light/dark theme in Rofi

--- home/scripts/wallpaper.sh
+++ home/scripts/wallpaper.sh
@@ -3,8 +3,14 @@
 path=$(~/scripts/private/unsplash.sh)
 h=$(date '+%H')
 
+# pywal generates a "light" and "dark" Rofi theme, which is rather in contrast
+# to the dynamism supported by the "-l" flag. We'll create our own symbolic
+# link to keep it in sync after running pywal itself.
 if [[ "$h" -gt 04 && "$h" -lt 17 ]]; then
   wal -l -i "$path"
+  ln -sf ~/.cache/wal/colors-rofi-dark.rasi ~/.cache/wal/colors-rofi.rasi
 else
   wal -i "$path"
+  ln -sf ~/.cache/wal/colors-rofi-light.rasi ~/.cache/wal/colors-rofi.rasi
 fi
+
