ccbed22c9d5e9e9b6a34cec3b545abecfd27dbc1
Sam A. Horvath-Hunt <hello@samhh.com>
2021-11-11 14:45:29 +0000

    Fix EFM import-sort formatting not operating upon unsaved buffer

---
+++ home/scripts/import-sort-stdin.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+code="$(< /dev/stdin)"
+path="$1"
+
+# import-sort uses the file extension to determine how to format. It uses other
+# dependencies in node_modules/ too so our proxy file needs to be in the same
+# directory.
+ext=$(echo "$path" | sed 's/^.*\.//')
+proxy="$path.import-sort.tmp.$ext"
+
+# import-sort doesn't print to stdout if there's nothing to do, so using --write
+# on our proxy file which if nothing else contains unmodified code avoids some
+# awkward conditionals.
+echo "$code" > "$proxy"
+./node_modules/.bin/import-sort --write "$proxy"
+cat "$proxy"
+rm "$proxy"
