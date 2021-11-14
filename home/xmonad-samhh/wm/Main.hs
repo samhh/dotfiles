b64eebd4a55a14ddc55f49d2e5822db111d07882
Sam A. Horvath-Hunt <hello@samhh.com>
2021-11-14 15:10:44 +0000

    Style selection windows according to pywal

--- home/xmonad-samhh/wm/Main.hs
+++ home/xmonad-samhh/wm/Main.hs
@@ -3,8 +3,11 @@
 module Main (main) where
 
 import           App                         (apps)
-import           Color                       (Palette (color0, color3),
-                                              getColorOrHideous, getTheme)
+import           Color                       (HexColor,
+                                              Palette (color0, color3, color1),
+                                              Specials (background, foreground),
+                                              getColorOrHideous,
+                                              getSpecialOrHideous, getTheme)
 import           Data.Default                (def)
 import qualified Data.Map                    as M
 import           Function                    (bindM2)
@@ -35,8 +38,9 @@ import           XMonad                      (ChangeLayout (NextLayout),
                                               windows, withFocused, (.|.), (=?))
 import qualified XMonad
 import           XMonad.Actions.CopyWindow   (copyToAll, killAllOtherCopies)
-import           XMonad.Actions.EasyMotion   (ChordKeys (AnyKeys), cancelKey,
-                                              sKeys, selectWindow)
+import           XMonad.Actions.EasyMotion   (ChordKeys (AnyKeys),
+                                              EasyMotionConfig (bgCol, txtCol, borderCol, borderPx),
+                                              cancelKey, sKeys, selectWindow)
 import           XMonad.Config.Desktop       (desktopConfig)
 import           XMonad.Hooks.InsertPosition (Focus (..), Position (..),
                                               insertPosition)
@@ -69,12 +73,19 @@ browserTarget x = spaceHasBrowser x <&> \case
 spawnWithBrowserTarget :: BrowserProfile -> Spawn -> X ()
 spawnWithBrowserTarget x y = spawn . (toSpawnable y <>) . (" " <>) =<< browserTarget x
 
-selectWindow' :: X (Maybe Window)
-selectWindow' = selectWindow $ def { sKeys = AnyKeys colemakHomeKeys, cancelKey = K.xK_Escape }
+selectWindow' :: ((Specials -> HexColor) -> HexColor) -> ((Palette -> HexColor) -> HexColor) -> X (Maybe Window)
+selectWindow' s c = selectWindow $ def
+  { sKeys = AnyKeys colemakHomeKeys
+  , cancelKey = K.xK_Escape
+  , bgCol = s background
+  , txtCol = s foreground
+  , borderCol = c color1
+  , borderPx = 3
+  }
   where colemakHomeKeys = [K.xK_t, K.xK_n, K.xK_s, K.xK_e, K.xK_r, K.xK_i]
 
-onSelectWindow :: (Window -> X ()) -> X ()
-onSelectWindow f = (`whenJust` f) =<< selectWindow'
+onSelectWindow :: ((Specials -> HexColor) -> HexColor) -> ((Palette -> HexColor) -> HexColor) -> (Window -> X ()) -> X ()
+onSelectWindow s c f = (`whenJust` f) =<< selectWindow' s c
 
 config t = desktopConfig
   { terminal = "alacritty"
@@ -91,7 +102,7 @@ config t = desktopConfig
   , keys = \cfg@XConfig {XMonad.modMask = super, XMonad.terminal = term} ->
       M.fromList $
         [ ((super, K.xK_Return), spawn term)
-        , ((super .|. K.shiftMask, K.xK_q), onSelectWindow killWindow)
+        , ((super .|. K.shiftMask, K.xK_q), onSelectWindow' killWindow)
         , ((super .|. K.shiftMask .|. K.controlMask, K.xK_q), kill)
         , ((super, K.xK_Down), windows W.focusDown)
         , ((super, K.xK_Up), windows W.focusUp)
@@ -111,7 +122,7 @@ config t = desktopConfig
         , ((super, K.xK_s), withFocused . toggleFloat $ centreRect)
         , ((super, K.xK_a), windows copyToAll <> withFocused (enableFloat' videoRect))
         , ((super .|. K.shiftMask, K.xK_a), killAllOtherCopies <> withFocused disableFloat')
-        , ((super, K.xK_h), onSelectWindow (windows . W.focusWindow))
+        , ((super, K.xK_h), onSelectWindow' (windows . W.focusWindow))
         , ((super, K.xK_o), spawn' CloseNotif)
         , ((super .|. K.shiftMask, K.xK_o), spawn' CloseAllNotifs)
         , ((K.nomod, K.xK_VolDown), spawn' DecVol)
@@ -142,3 +153,5 @@ config t = desktopConfig
           <> (workspaceSwap super <$> Workspaces.workspaces)
   }
   where c = getColorOrHideous t
+        s = getSpecialOrHideous t
+        onSelectWindow' = onSelectWindow s c
