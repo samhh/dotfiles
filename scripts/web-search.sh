dbbf2e0bf0545e10e7d6803d060efdcbb382677f
Sam A. Horvath-Hunt <hello@samhh.com>
2022-03-27 15:32:57 +0100

    Remove bash dependency from web search script

--- scripts/web-search.sh
+++ scripts/web-search.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 target=${1:-'tab'}
 
 query=$(echo '' | bemenu 0 -p web)
-[[ -n $query ]] || exit
+if [ -z $query ]; then exit 1; fi
 
 qutebrowser --target "$target" --untrusted-args "$query"
