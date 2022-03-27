867dd3b4d4b3942a0aae91699a0e62bc6e1d5194
Sam A. Horvath-Hunt <hello@samhh.com>
2022-03-27 22:13:13 +0100

    Patch config permissions check out of aerc

--- email.nix
+++ email.nix
@@ -2,6 +2,12 @@
 
 let
   uname = "sam";
+
+  # Patched not to check the permissions of `accounts.conf` which is managed by
+  # Home Manager below.
+  aerc = pkgs.aerc.overrideAttrs (attrs: {
+    patches = attrs.patches ++ [ ./patches/aerc-no-perms-check.patch ];
+  });
 in {
   home-manager.users.${uname} = {
     xdg.configFile.aerc.source = ./cfg/aerc;
