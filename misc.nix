1ca7562b45600305398d5622046846d3058b02f5
Sam A. Horvath-Hunt <hello@samhh.com>
2022-05-20 17:45:46 +0100

    Remove distrobox override
    We're 1.2.15 now.

--- misc.nix
+++ misc.nix
@@ -2,16 +2,6 @@
 
 let
   home = config.users.users.${uname}.home;
-
-  # Contains this fix for Arch (not in <=1.2.14):
-  #   https://github.com/89luca89/distrobox/issues/221
-  distrobox = pkgs.distrobox.overrideAttrs (attrs: rec {
-    version = "e6342a05c3cc83d612bd05934a2025cceb86af32";
-    src = builtins.fetchurl {
-      url = "https://github.com/89luca89/distrobox/archive/${version}.tar.gz";
-      sha256 = "1ama58gv2cw48pxic9fjhjv4vvxj813i7wigb238jsdbqxryr4qp";
-    };
-  });
 in {
   # Fixes Teensy loader flashing Ergodox EZ:
   #   https://github.com/zsa/docs/issues/14
