a7f24795ff8ced465955c7aaf8f0e2c72e53151e
Sam A. Horvath-Hunt <hello@samhh.com>
2022-05-20 18:59:16 +0100

    Localise bindings to better support multiple hosts

--- flake.nix
+++ flake.nix
@@ -10,44 +10,43 @@
     };
 
   outputs = { nixpkgs, home-manager, ... }:
-    let
-      hostName = "alakazam";
-      system = "x86_64-linux";
+    {
+      nixosConfigurations = {
+        alakazam =
+          let
+            system = "x86_64-linux";
+            pkgs = import nixpkgs {
+              inherit system;
 
-      pkgs = import nixpkgs {
-        inherit system;
+              config.allowUnfree = true;
+            };
+          in nixpkgs.lib.nixosSystem {
+            inherit pkgs system;
 
-        config.allowUnfree = true;
-      };
+            modules = [
+              home-manager.nixosModules.home-manager {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+              }
+              ./hosts/alakazam
+            ];
 
-      lib = nixpkgs.lib;
-    in {
-      nixosConfigurations.${hostName} = lib.nixosSystem {
-        inherit pkgs system;
+            specialArgs = {
+              hostName = "alakazam";
 
-        modules = [
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-          }
-          ./hosts/${hostName}
-        ];
+              uname = "sam";
+              email = "hello@samhh.com";
 
-        specialArgs = {
-          inherit hostName;
+              emailPassPath = "emails/migadu.com/mailbox/hello";
+              nasPath = "/mnt/nas";
 
-          uname = "sam";
-          email = "hello@samhh.com";
-
-          emailPassPath = "emails/migadu.com/mailbox/hello";
-          nasPath = "/mnt/nas";
-
-          termBin = "${pkgs.foot}/bin/foot";
-          editorBin = "${pkgs.neovim}/bin/nvim";
-          launcherBin = "${pkgs.dmenu}/bin/dmenu";
-          webBrowserBin = "${pkgs.qutebrowser}/bin/qutebrowser";
-          streamerBin = "${pkgs.streamlink}/bin/streamlink";
-        };
+              termBin = "${pkgs.foot}/bin/foot";
+              editorBin = "${pkgs.neovim}/bin/nvim";
+              launcherBin = "${pkgs.dmenu}/bin/dmenu";
+              webBrowserBin = "${pkgs.qutebrowser}/bin/qutebrowser";
+              streamerBin = "${pkgs.streamlink}/bin/streamlink";
+            };
+          };
       };
     };
 }
