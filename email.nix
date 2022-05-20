594f99a6be4742d2db1299883ecc72febd4197c2
Sam A. Horvath-Hunt <hello@samhh.com>
2022-05-20 17:42:04 +0100

    Remove aerc permissions patch/workaround
    New option included as of 0.10.0.

--- email.nix
+++ email.nix
@@ -1,12 +1,6 @@
 { pkgs, uname, ... }:
 
-let
-  # Patched not to check the permissions of `accounts.conf` which is managed by
-  # Home Manager below.
-  aerc = pkgs.aerc.overrideAttrs (attrs: {
-    patches = attrs.patches ++ [ ./patches/aerc-no-perms-check.patch ];
-  });
-in {
+{
   home-manager.users.${uname} = {
     xdg.configFile.aerc.source = ./cfg/aerc;
 
