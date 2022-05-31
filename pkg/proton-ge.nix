6dc527cefc7975cc6236f3a9f511272a92feaa27
Sam A. Horvath-Hunt <hello@samhh.com>
2022-05-31 18:04:20 +0100

    Bump Proton-GE
    A sha512 checksum is produced in CI alongside the binary, so let's just use that.

--- pkg/proton-ge.nix
+++ pkg/proton-ge.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "proton-ge-custom";
-  version = "GE-Proton7-10";
+  version = "GE-Proton7-20";
 
   src = fetchurl {
     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
-    sha256 = "0i7m4cyj47ggk0gc6argsvpxfyagx4q4258dlrf968dvi9wxd6xf";
+    sha512 = "b3e88e4013b725ad90a3178e064cf038bd2b2afac30acaf963ff3d5240a68755700c26afb61393b71e34d51d5cef330a180206d4ac97cc603305e64c2b1cf7db";
   };
 
   installPhase = ''
