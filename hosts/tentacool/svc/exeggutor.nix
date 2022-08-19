4b67dd9e08bd41b30c14c35fb1b7b1dac60d1602
Sam A. Horvath-Hunt <hello@samhh.com>
2022-08-19 11:27:04 +0100

    Refer to Zigbee coordinator by ID

--- hosts/tentacool/svc/exeggutor.nix
+++ hosts/tentacool/svc/exeggutor.nix
@@ -31,7 +31,7 @@ in
     settings = {
       homeassistant = true;
       permit_join = false;
-      serial.port = "/dev/ttyUSB0";
+      serial.port = "/dev/serial/by-id/usb-ITead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_803e9325733bec11987b9e957a0af07f-if00-port0";
       frontend = true;
     };
   };
