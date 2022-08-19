5c376d35a7caf52cfb3e2e5a48f99e78846cfe72
Sam A. Horvath-Hunt <hello@samhh.com>
2022-08-19 11:21:09 +0100

    Proton GE: 7-20 -> 7-29

--- pkg/proton-ge.nix
+++ pkg/proton-ge.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "proton-ge-custom";
-  version = "GE-Proton7-20";
+  version = "GE-Proton7-29";
 
   src = fetchurl {
     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
-    sha512 = "b3e88e4013b725ad90a3178e064cf038bd2b2afac30acaf963ff3d5240a68755700c26afb61393b71e34d51d5cef330a180206d4ac97cc603305e64c2b1cf7db";
+    sha512 = "b4c7a6894302a302a15b6f3968987c98190461e9cbb4afa7738fe28fb24b1a2730140c424efc60a77624c8867bcc52e3b6ae1a60c9246aedf208ddb994d602d5";
   };
 
   installPhase = ''
