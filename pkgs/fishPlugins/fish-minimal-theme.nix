def2692e3d89020b2d406c1827096cfe967e3afd
Sam A. Horvath-Hunt <hello@samhh.com>
2022-08-29 18:02:16 +0100

    Update fish-minimal-theme
    Performance in repos like nixpkgs is drastically improved.

--- pkgs/fishPlugins/fish-minimal-theme.nix
+++ pkgs/fishPlugins/fish-minimal-theme.nix
@@ -2,10 +2,10 @@
 
 pkgs.fishPlugins.buildFishPlugin rec {
   pname = "fish-minimal-theme";
-  version = "9cabe0f044bb80bcbfec7d6804971836003df681";
+  version = "f1d159b9d6b6f212f4de143afc743eef45a29533";
 
   src = builtins.fetchTarball {
     url = "https://github.com/samhh/fish-minimal-theme/archive/${version}.tar.gz";
-    sha256 = "1ilmnjxsaqzkjlqdn2m348bfjg23k6dkcak5p7qb84yz13pf3dfv";
+    sha256 = "07nfnajmvhr3908gvjgx83rfnxm5pnvri6ahybyqhs7cqpnl8h4q";
   };
 }
