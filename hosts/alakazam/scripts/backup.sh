9a20fb5bc1b14ba8fe97efce381caf9e20600caf
Sam A. Horvath-Hunt <hello@samhh.com>
2022-08-31 19:03:02 +0100

    Add Tentacool container backups to script
    These backups don't benefit from versioning given the additional
    complexity that'd entail.

--- hosts/alakazam/scripts/backup.sh
+++ hosts/alakazam/scripts/backup.sh
@@ -10,6 +10,20 @@ tree /mnt/nas/movies/ > /mnt/nas/logs/movies.txt
 "$(dirname "$0")"/seedbox/sonarr.sh > /mnt/nas/logs/sonarr.txt
 "$(dirname "$0")"/seedbox/radarr.sh > /mnt/nas/logs/radarr.txt
 
+echo "Preparing Tentacool service backups..."
+ssh -t sam@tentacool \
+  "
+    rm -rf ~/backups/ &&
+    mkdir -p ~/backups/ &&
+
+    sudo podman volume export hass > ~/backups/starmie.tar &&
+    tar -f ~/backups/starmie.tar --wildcards --delete 'home-assistant_v2.*' &&
+
+    sudo podman exec pihole sh -c 'pihole -a -t && mv /pi-hole-tentacool-teleporter_*.tar.gz /backup.tar.gz' &&
+    sudo podman cp pihole:/backup.tar.gz ~/backups/onix.tar.gz
+  "
+scp -r sam@tentacool:"~/backups/*" "/mnt/nas/archive/tentacool/"
+
 echo "Syncing mail..."
 offlineimap
 
