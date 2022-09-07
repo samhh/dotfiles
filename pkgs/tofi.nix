0e62a45ff5d39df15a739f125aa15be26efd6315
Sam A. Horvath-Hunt <hello@samhh.com>
2022-09-07 15:25:39 +0100

    Update tofi package
    Now includes support for space characters with --require-match false (#30).

--- pkgs/tofi.nix
+++ pkgs/tofi.nix
@@ -3,11 +3,11 @@
 stdenv.mkDerivation rec {
   pname = "tofi";
   # Latest release doesn't yet contain --require-match or --prompt-padding.
-  version = "e72f81799b1184d6b1b1bf8f70ec32d9cc139556";
+  version = "adc683b7fa847bc1776e3bece532c39aff12b039";
 
   src = fetchurl {
     url = "https://github.com/philj56/tofi/archive/${version}.tar.gz";
-    sha256 = "1j5n6cmcyqgwbri4w50gid7v6p66fdddfna1g3iclrc1yqnzbyhz";
+    sha256 = "192g1qwhnfhqcmmzv08frdkqwwi7z40nc6axhplvcmgn0mp524z9";
   };
 
   buildInputs = with pkgs; [ cairo harfbuzz libxkbcommon meson ninja pango pkg-config scdoc wayland wayland-protocols ];
