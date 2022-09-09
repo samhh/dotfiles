5227a7d7d4ed14f24760ab82182451d08ffb4fcd
Sam A. Horvath-Hunt <hello@samhh.com>
2022-09-09 11:39:41 +0100

    Upgrade eyeliner.nvim

--- pkgs/vimPlugins/eyeliner-nvim.nix
+++ pkgs/vimPlugins/eyeliner-nvim.nix
@@ -3,7 +3,7 @@
 pkgs.vimUtils.buildVimPlugin {
   name = "eyeliner.nvim";
   src = builtins.fetchTarball {
-    url = "https://github.com/jinh0/eyeliner.nvim/archive/7b8ce8c1e0b466328e02e7649c759dc96ed457aa.tar.gz";
-    sha256 = "08l7gyf5g4cw47hxnjph6g8r3ldvzwv4z9czzyf14rn0glri0hwb";
+    url = "https://github.com/jinh0/eyeliner.nvim/archive/f13c8a19bd23db6ead7424b29f064279f3546738.tar.gz";
+    sha256 = "0cb5vsh6ivlwndbaq2ym0rymaziwjnjw2aw22jwzpkyd2162rwzy";
   };
 }
