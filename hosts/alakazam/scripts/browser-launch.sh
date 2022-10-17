0a0bc76c4296a7e44dc220392ed9aa246d0c4d3b
Sam A. Horvath-Hunt <hello@samhh.com>
2022-10-17 17:49:01 +0100

    Add Trello case to browser launch script

--- hosts/alakazam/scripts/browser-launch.sh
+++ hosts/alakazam/scripts/browser-launch.sh
@@ -7,7 +7,7 @@ query="$1"
 profile="$2"
 
 isUnsplashAddr=$(case "$query" in
-  *figma.com*|*linear.app*|*gettyimages*) echo "yep" ;;
+  *figma.com*|*linear.app*|*trello.com*|*gettyimages*) echo "yep" ;;
 esac)
 
 if [ "$isUnsplashAddr" ]; then
