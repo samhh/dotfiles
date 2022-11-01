28459daa8ea6d0a6669a406680213af8fd4e1777
Sam A. Horvath-Hunt <hello@samhh.com>
2022-11-01 12:33:08 +0000

    tofi: 0.6.0 -> 0.7.0

--- pkgs/tofi.nix
+++ pkgs/tofi.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "tofi";
-  version = "0.6.0";
+  version = "0.7.0";
 
   src = fetchurl {
     url = "https://github.com/philj56/tofi/archive/refs/tags/v${version}.tar.gz";
-    sha256 = "05jh65klqzbgszgyq6a1cllskk54acibkvz2xvnqnyxf0b9dv8v5";
+    sha256 = "1kb3ivbvn4ryd1m8a7zcr8xc0flgvyya4wbdyqigcd14ffxq0imq";
   };
 
   buildInputs = with pkgs; [ cairo harfbuzz libxkbcommon meson ninja pango pkg-config scdoc wayland wayland-protocols ];
