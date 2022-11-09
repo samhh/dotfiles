4c47f944224cd00bef1024e58e12d06e7fd7d104
Sam A. Horvath-Hunt <hello@samhh.com>
2022-11-09 18:07:35 +0000

    Expose Magnemite

--- hosts/tentacool/svc/magnemite.nix
+++ hosts/tentacool/svc/magnemite.nix
@@ -1,8 +1,23 @@
 { ... }:
 
+let
+  name = "magnemite";
+  lanPort = 9696;
+in
 {
   services.prowlarr = {
     enable = true;
     openFirewall = true;
   };
+
+  services.nginx.virtualHosts."${name}.samhh.com" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:${toString lanPort}";
+      proxyWebsockets = true;
+    };
+  };
+
+  services.ddclient.domains = [ name ];
 }
