030d4412e0527bdf27e2abce6c3797cda4e740eb
Sam A. Horvath-Hunt <hello@samhh.com>
2022-11-09 16:57:42 +0000

    Revert "Pi-hole: 2022.09.4 -> 2022.10"
    This reverts commit 49850e402d257d429a7e8c8f37fd8649fcd9e153.
    
    Currently blocked by an inability to deploy to Tentacool due to
    nameserver issues, see #364.

--- hosts/tentacool/svc/onix.nix
+++ hosts/tentacool/svc/onix.nix
@@ -14,7 +14,7 @@ in
   ];
 
   virtualisation.oci-containers.containers.pihole = {
-    image = "pihole/pihole:2022.10";
+    image = "pihole/pihole:2022.09.4";
     volumes = [
       "pihole:/etc/pihole"
       "dnsmasq:/etc/dnsmasq.d"
