dfb8034f4a66fe1203f458d193891c8e6f3eb0a5
Sam A. Horvath-Hunt <hello@samhh.com>
2022-11-15 21:36:01 +0000

    Theme terminal w/ nix-colors

--- hosts/alakazam/terminal.nix
+++ hosts/alakazam/terminal.nix
@@ -23,28 +23,34 @@ in
         colors = {
           alpha = .92;
 
-          # From: https://github.com/rebelot/kanagawa.nvim/blob/master/extras/foot_kanagawa.ini
-          foreground = "dcd7ba";
-          # background = "1f1f28";
-          selection-foreground = "c8c093";
+          # Non-base 16 from:
+          #   https://github.com/rebelot/kanagawa.nvim/blob/f3c1bb8b6f67b26a2d39babf86d0807c4004c28e/extras/kanagawa.conf
+          #
+          # As per:
+          #   https://github.com/rebelot/kanagawa.nvim/issues/58#issuecomment-1184386932
+          #
+          # Except the background, which is ignored. I prefer it without the
+          # blueish hue.
+          foreground = config.colorScheme.colors.base05;
+          selection-foreground = config.colorScheme.colors.base04;
           selection-background = "2d4f67";
           regular0 = "090618";
-          regular1 = "c34043";
+          regular1 = config.colorScheme.colors.base08;
           regular2 = "76946a";
           regular3 = "c0a36e";
-          regular4 = "7e9cd8";
-          regular5 = "957fb8";
+          regular4 = config.colorScheme.colors.base0D;
+          regular5 = config.colorScheme.colors.base0E;
           regular6 = "6a9589";
-          regular7 = "c8c093";
-          bright0 = "727169";
+          regular7 = config.colorScheme.colors.base04;
+          bright0 = config.colorScheme.colors.base03;
           bright1 = "e82424";
-          bright2 = "98bb6c";
+          bright2 = config.colorScheme.colors.base0B;
           bright3 = "e6c384";
-          bright4 = "7fb4ca";
-          bright5 = "938aa9";
+          bright4 = config.colorScheme.colors.base0C;
+          bright5 = config.colorScheme.colors.base06;
           bright6 = "7aa89f";
-          bright7 = "dcd7ba";
-          "16" = "ffa066";
+          bright7 = config.colorScheme.colors.base05;
+          "16" = config.colorScheme.colors.base09;
           "17" = "ff5d62";
         };
       };
