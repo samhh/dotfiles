d6233f5854ecac6029e03af882ad152c9c244a3f
Sam A. Horvath-Hunt <hello@samhh.com>
2022-12-12 16:45:51 +0000

    Add 418 default to Tentacool

--- hosts/tentacool/http.nix
+++ hosts/tentacool/http.nix
@@ -17,5 +17,13 @@
     enable = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
+
+    # When nginx can't match a request to a vhost it defaults to the first
+    # defined block. We can access that by connecting directly to this host's
+    # IP. This isn't really important, but it's a bit messy.
+    #
+    # Nix writes the vhosts to the config in alphabetical order, hence the name
+    # of this vhost.
+    virtualHosts."_".locations."/".return = "418";
   };
 }
