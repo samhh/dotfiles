e8741648e1e668a2a58aef75bba03c03c1a56f13
Sam A. Horvath-Hunt <hello@samhh.com>
2023-01-30 15:22:46 +0000

    Remove unused eyeliner-nvim module

--- pkgs/vimPlugins/default.nix
+++ pkgs/vimPlugins/default.nix
@@ -2,7 +2,6 @@
 
 {
   exrc-vim = pkgs.callPackage ./exrc-vim.nix { };
-  eyeliner-nvim = pkgs.callPackage ./eyeliner-nvim.nix { };
   nvim-surround = pkgs.callPackage ./nvim-surround.nix { };
   vim-just = pkgs.callPackage ./vim-just.nix { };
 }
