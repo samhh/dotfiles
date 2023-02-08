363e65e79945c51ec77e561955c38558ff7de2e4
Sam A. Horvath-Hunt <hello@samhh.com>
2023-02-08 11:26:39 +0000

    Fix DDNS

--- hosts/tentacool/ddns.nix
+++ hosts/tentacool/ddns.nix
@@ -10,5 +10,11 @@
     protocol = "namecheap";
     username = "samhh.com";
     passwordFile = config.age.secrets.ddns-token.path;
+    # The default provider is currently broken for SSL:
+    #   https://github.com/ddclient/ddclient/issues/309
+    #
+    # A list of alternative providers is available here:
+    #   https://github.com/ddclient/ddclient/blob/215d4679e4640c5975dd3983d3e2c0ebe7b0e140/ddclient.in#L107-L121
+    use = "web, web=googledomains";
   };
 }
