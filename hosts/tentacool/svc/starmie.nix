13245da5eb8351732675d112e0f4bd8907ef9a84
Sam A. Horvath-Hunt <hello@samhh.com>
2023-03-02 16:59:08 +0000

    HASS: 2023.2.2 -> 2023.3.0

--- hosts/tentacool/svc/starmie.nix
+++ hosts/tentacool/svc/starmie.nix
@@ -21,7 +21,7 @@ in
   };
 
   virtualisation.oci-containers.containers.hass = {
-    image = "ghcr.io/home-assistant/home-assistant:2023.2.2";
+    image = "ghcr.io/home-assistant/home-assistant:2023.3.0";
     volumes = [
       "hass:/config"
     ];
