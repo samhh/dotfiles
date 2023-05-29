c6afcbd1284325e26b7b535ca5ef1396c16d9d20
Sam A. Horvath-Hunt <hello@samhh.com>
2023-05-29 15:11:08 +0100

    Mount Snorlax by IP

--- nixos/snorlax.nix
+++ nixos/snorlax.nix
@@ -1,8 +1,13 @@
 { config, ... }:
 
+# Mount by IP rather than hostname. This is for Tentacool, on which DNS is
+# hosted, which won't have access to its own DNS at boot when it tries to mount
+# Snorlax over the network.
+let ip = "192.168.0.83";
+in
 {
   fileSystems.${config.nas.path} = {
-    device = "snorlax:/volume1/media/";
+    device = "${ip}:/volume1/media/";
     fsType = "nfs";
     options = [ "nfsvers=4" "noauto" "x-systemd.automount" ];
   };
