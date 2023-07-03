1c8d6b11ce7891f0e689fce04d002b1f2de7da35
Sam A. Horvath-Hunt <hello@samhh.com>
2023-07-03 18:53:17 +0100

    Add temperature sensors to Mangohud

--- hosts/alakazam/gaming.nix
+++ hosts/alakazam/gaming.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   programs.steam.enable = true;
@@ -6,10 +6,20 @@
   home-manager.users.${config.username} = {
     home.sessionVariables.STEAM_EXTRA_COMPAT_TOOLS_PATHS = pkgs.proton-ge;
 
+    programs.mangohud = {
+      enable = true;
+      settings =
+        let
+          # This is additive atop the defaults.
+          sensors = [ "cpu_temp" "gpu_temp" ];
+          f = x: { name = x; value = true; };
+        in
+        lib.listToAttrs (map f sensors);
+    };
+
     home.packages = with pkgs; [
       gamescope
       lutris-free
-      mangohud
       prismlauncher
       proton-ge
     ];
