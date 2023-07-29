67d0549726ac97f2212a0eaca5d211952e931726
Sam A. Horvath-Hunt <hello@samhh.com>
2023-07-29 16:50:06 +0100

    Remove dead keyserver

--- hosts/alakazam/scripts/sendkey.sh
+++ hosts/alakazam/scripts/sendkey.sh
@@ -5,7 +5,6 @@ key="B7C77CD313EAE76366FE1D7E4667250BD56735A8"
 # These'll be separated at newlines, hence the awkward formatting.
 servers="keys.openpgp.org
 pgp.mit.edu
-pool.sks-keyservers.net
 keyserver.ubuntu.com"
 
 echo "$servers" | tr ' ' '\n' | while read -r server; do
