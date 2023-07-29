e7e7e6589c2c1a59f7bd29a9661632de612c942b
Sam A. Horvath-Hunt <hello@samhh.com>
2023-07-29 22:09:16 +0100

    DRY

--- pkgs/vimPlugins/exrc-vim.nix
+++ pkgs/vimPlugins/exrc-vim.nix
@@ -1,10 +1,10 @@
 { pkgs }:
 
-pkgs.vimUtils.buildVimPlugin {
+pkgs.vimUtils.buildVimPlugin rec {
   pname = "exrc.vim";
   version = "ae734ae2c087b370d869e41a2706a128d8f3fc37";
   src = builtins.fetchTarball {
-    url = "https://github.com/ii14/exrc.vim/archive/ae734ae2c087b370d869e41a2706a128d8f3fc37.tar.gz";
+    url = "https://github.com/ii14/exrc.vim/archive/${version}.tar.gz";
     sha256 = "0jadpcg3hsfzbglh21zlfhj2d9ymyh73p3kd4wd9imlhdhsx99d7";
   };
   # Tests run via `make` will fail as they're expecting `vim`.
