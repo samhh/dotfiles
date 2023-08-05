a6d4e5b16c87b53fb8a2bf21bb05370bc6e0f533
Sam A. Horvath-Hunt <hello@samhh.com>
2023-08-05 17:07:06 +0100

    Define and set custom config separately

--- cfg/default.nix
+++ cfg/default.nix
@@ -5,7 +5,6 @@
 
 with lib; {
   options = {
-    # System-agnostic.
     username = mkOption { type = types.str; };
     fullName = mkOption { type = types.str; };
     email = {
@@ -18,22 +17,10 @@ with lib; {
         description = "Files which you'd typically want excluded, such as metadata and other dotfiles.";
       };
     };
-
-    # Potentially system-specific.
     apps = {
       terminal.bin = mkOption { type = types.str; };
       launcher.bin = mkOption { type = types.str; };
       webBrowser.bin = mkOption { type = types.str; };
-    };
-  };
-
-  config = {
-    username = "sam";
-    fullName = "Sam A. Horvath-Hunt";
-    email.address = "hello@samhh.com";
-    nas = {
-      path = "/mnt/nas";
-      hiddenFiles = [ "@eaDir" ".DS_Store" ];
     };
   };
 }
--- common/config.nix
+++ common/config.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }:
 
 {
+  username = "sam";
+  fullName = "Sam A. Horvath-Hunt";
+  email.address = "hello@samhh.com";
+  nas = {
+    path = "/mnt/nas";
+    hiddenFiles = [ "@eaDir" ".DS_Store" ];
+  };
   apps = {
     terminal.bin = "${pkgs.foot}/bin/foot";
     launcher.bin = "${pkgs.tofi}/bin/tofi";
