139d1a2fc75be5a58c25817e90ee23223d4d9a7b
Sam A. Horvath-Hunt <hello@samhh.com>
2023-08-05 16:59:56 +0100

    Add flake-parts shell template

--- templates/default.nix
+++ templates/default.nix
@@ -1,4 +1,9 @@
 {
+  devshell-flake-parts = {
+    path = ./devshell-flake-parts;
+    description = "Minimal dev shell via flake-parts";
+  };
+
   devshell-flake-utils = {
     path = ./devshell-flake-utils;
     description = "Minimal dev shell via flake-utils";
---
+++ templates/devshell-flake-parts/flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+
+      perSystem = { pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            git
+          ];
+        };
+      };
+    };
+}
