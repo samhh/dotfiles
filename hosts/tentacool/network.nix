c8d2a1335608db1d71e1d845ab4c876440107d15
Sam A. Horvath-Hunt <hello@samhh.com>
2023-08-31 18:27:54 +0100

    Define static IPv6 token/suffix for Tentacool
    Alongside the relevant network firewall configuration this makes the
    machine publically accessible with a stable suffix. The prefix, at least
    in my case, comes from my ISP. Stability here refers to the fact that
    absent a static configuration like this, hardware changes affecting the
    MAC address would also affect the IPv6 address.

--- hosts/tentacool/network.nix
+++ hosts/tentacool/network.nix
@@ -3,5 +3,19 @@
 {
   networking = {
     hostName = "tentacool";
+    dhcpcd.enable = false;
+  };
+
+  systemd.network = {
+    enable = true;
+    networks."10-lan" = {
+      matchConfig.Name = "eno1";
+      networkConfig = {
+        DHCP = "ipv4";
+        IPv6AcceptRA = true;
+      };
+      ipv6AcceptRAConfig.Token = "static:::1:2:3:4";
+      linkConfig.RequiredForOnline = "routable";
+    };
   };
 }
