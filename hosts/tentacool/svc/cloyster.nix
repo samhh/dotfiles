b59082827de0b939007aa4d49fc52a94a1061504
Sam A. Horvath-Hunt <hello@samhh.com>
2023-09-08 15:26:47 +0100

    scrypted: 18-jammy-thin-v0.41.0 -> 18-jammy-thin-v0.48.0

--- hosts/tentacool/svc/cloyster.nix
+++ hosts/tentacool/svc/cloyster.nix
@@ -11,7 +11,7 @@ in
   };
 
   virtualisation.oci-containers.containers.scrypted = {
-    image = "koush/scrypted:18-jammy-thin-v0.41.0";
+    image = "koush/scrypted:18-jammy-thin-v0.48.0";
     volumes = [
       "scrypted:/server/volume"
     ];
