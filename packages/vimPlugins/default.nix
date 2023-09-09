945c351a6734dc3d729a479c18e27eab7cb3323f
Sam A. Horvath-Hunt <hello@samhh.com>
2023-09-09 14:31:43 +0100

    Use nixpkgs nvim-surround

--- packages/vimPlugins/default.nix
+++ packages/vimPlugins/default.nix
@@ -2,7 +2,6 @@
 
 {
   exrc-vim = pkgs.callPackage ./exrc-vim.nix { };
-  nvim-surround = pkgs.callPackage ./nvim-surround.nix { };
   vim-just = pkgs.callPackage ./vim-just.nix { };
   vim-lumen = pkgs.callPackage ./vim-lumen.nix { };
 }
