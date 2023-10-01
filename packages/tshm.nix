74bc5d40fb0a50f4294bda348e23dd8fbdc822cb
Sam A. Horvath-Hunt <hello@samhh.com>
2023-10-01 17:28:21 +0100

    Add Linux support back to tshm derivation

--- packages/tshm.nix
+++ packages/tshm.nix
@@ -4,10 +4,18 @@ stdenv.mkDerivation rec {
   pname = "tshm";
   version = "0.4.2";
 
-  src = fetchurl {
-    url = "https://github.com/samhh/tshm/releases/download/${version}/tshm-${version}-macos-x86_64";
-    sha256 = "0mrw84pz4hjx6rzzmnj1kzvz8wgh0hxpviq3f9gj7wg7c1hvickm";
-  };
+  src =
+    if stdenv.isDarwin
+    then
+      fetchurl
+        {
+          url = "https://github.com/samhh/tshm/releases/download/${version}/tshm-${version}-macos-x86_64";
+          sha256 = "0mrw84pz4hjx6rzzmnj1kzvz8wgh0hxpviq3f9gj7wg7c1hvickm";
+        } else
+      fetchurl {
+        url = "https://github.com/samhh/tshm/releases/download/${version}/tshm-${version}-linux-x86_64";
+        sha256 = "0bcc029kricsnz3ixqknrnl99hgdi07nk5r8gzj0kz6d5xal2c3k";
+      };
 
   dontUnpack = true;
   dontStrip = true;
@@ -22,7 +30,7 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/samhh/tshm";
     description = "A parser and formatter for TypeScript declarations that outputs HM-style type signatures.";
     license = lib.licenses.mit;
-    # Assuming Rosetta.
-    platforms = [ "aarch64-darwin" "x86_64-darwin" ];
+    # Assuming Rosetta for aarch64-darwin.
+    platforms = [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ];
   };
 }
