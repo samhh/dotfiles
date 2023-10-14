47a8ec925b01f7a49fc17aea86276bd1facf7879
Sam A. Horvath-Hunt <hello@samhh.com>
2023-10-14 11:05:07 +0100

    Remove HASS & Zigbee2MQTT backups

--- homelab/svc/hass.nix
+++ homelab/svc/hass.nix
@@ -1,20 +1,9 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 let
   lanPort = 8123;
   mDNSPort = 5353;
   homekitBridgePort = 21064;
-
-  backup = pkgs.writeShellScript "hass-backup" ''
-    set -e
-
-    tmp=$(mktemp)
-
-    ${pkgs.podman}/bin/podman volume export hass > "$tmp"
-    ${pkgs.gnutar}/bin/tar -f "$tmp" --wildcards --delete 'home-assistant_v2.*'
-
-    mv "$tmp" ${config.nas.path}/backups/tentacool/hass.tar
-  '';
 in
 {
   networking.firewall = {
@@ -44,22 +33,4 @@ in
         extraOptions = [ hostNetworking ];
       };
     };
-
-  systemd = {
-    services."hass-backup" = {
-      description = "HASS backup";
-      wantedBy = [ "multi-user.target" ];
-      requires = [ "podman-hass.service" ];
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = backup;
-      };
-    };
-
-    timers."hass-backup" = {
-      description = "Run HASS backup";
-      wantedBy = [ "timers.target" ];
-      timerConfig.OnCalendar = "daily";
-    };
-  };
 }
--- homelab/svc/zigbee2mqtt.nix
+++ homelab/svc/zigbee2mqtt.nix
@@ -1,16 +1,6 @@
-{ config, pkgs, ... }:
+{ ... }:
 
-let
-  webPort = 8080;
-  backup = pkgs.writeShellScript "zigbee2mqtt-backup" ''
-    set -e
-
-    tmp=$(mktemp)
-
-    ${pkgs.gnutar}/bin/tar -c -C ${config.services.zigbee2mqtt.dataDir} --exclude ./log . > "$tmp"
-
-    mv "$tmp" ${config.nas.path}/backups/tentacool/zigbee2mqtt.tar
-  '';
+let webPort = 8080;
 in
 {
   networking.firewall.allowedTCPPorts = [
@@ -24,24 +14,6 @@ in
       permit_join = false;
       serial.port = "/dev/serial/by-id/usb-ITead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_803e9325733bec11987b9e957a0af07f-if00-port0";
       frontend = true;
-    };
-  };
-
-  systemd = {
-    services."zigbee2mqtt-backup" = {
-      description = "zigbee2mqtt backup";
-      wantedBy = [ "multi-user.target" ];
-      requires = [ "zigbee2mqtt.service" ];
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = backup;
-      };
-    };
-
-    timers."zigbee2mqtt-backup" = {
-      description = "Run zigbee2mqtt backup";
-      wantedBy = [ "timers.target" ];
-      timerConfig.OnCalendar = "daily";
     };
   };
 }
