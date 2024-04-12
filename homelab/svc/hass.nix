99d98283fa7d3d258bea3a94f93f16dc298d2e92
Sam A. Horvath-Hunt <hello@samhh.com>
2024-04-12 11:35:39 +0100

    HASS: 2024.4.0 -> 2024.4.2

--- homelab/svc/hass.nix
+++ homelab/svc/hass.nix
@@ -20,7 +20,7 @@ in
     in
     {
       hass = {
-        image = "ghcr.io/home-assistant/home-assistant:2024.4.0";
+        image = "ghcr.io/home-assistant/home-assistant:2024.4.2";
         volumes = [ "hass:/config" ];
         extraOptions = [ hostNetworking ];
         environment.TZ = "Europe/London";
