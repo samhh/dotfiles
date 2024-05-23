afd684320a3529c057ec6788fbf31fde5e73f2b7
Sam A. Horvath-Hunt <hello@samhh.com>
2024-05-23 17:38:43 +0100

    Alacritty -> Kitty
    Surprisingly Kitty has noticeably better latency, and that's with additional
    stuff like ligatures which Alacritty lacks.

--- home/terminal.nix
+++ home/terminal.nix
@@ -1,8 +1,25 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  programs.alacritty = {
+  programs.kitty = {
     enable = true;
     catppuccin.enable = true;
+    font = {
+      package = pkgs.hasklig;
+      name = "Hasklig Regular";
+    };
+    settings = {
+      remember_window_size = false;
+      enabled_layouts = "fat, grid";
+      visual_window_select_characters = "tnseriao";
+      scrollback_pager_history_size = "10";
+    };
+    keybindings = {
+      "cmd+w" = "discard_event";
+      "cmd+t" = "new_window";
+      "cmd+l" = "next_layout";
+      "cmd+e" = "focus_visible_window";
+      "cmd+f" = "show_scrollback";
+    };
   };
 }
