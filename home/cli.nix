993b21bf8bd5932d48ad2f3925ab479bfbcbded5
Sam A. Horvath-Hunt <hello@samhh.com>
2024-08-23 12:41:35 +0100

    Remove unused

--- home/cli.nix
+++ home/cli.nix
@@ -6,40 +6,25 @@
     catppuccin.enable = true;
   };
 
-  home.file."Library/Preferences/glow/glow.yml".text = ''
-    local: true
-    pager: true
-  '';
-
   home.packages = with pkgs; [
     ast-grep
     fd
     gdu
-    glow
     ripgrep
     sd
     tldr
     tre-command
-    nodePackages.yalc
 
     # Unsplash
     okta-aws-cli
   ];
 
   programs.git.ignores = [
-    # Yalc
-    ".yalc/"
-    "yalc.lock"
-
     # PureScript
     ".psc-ide-port"
     ".psci_modules/"
 
     # npm
     ".npmrc"
-
-    # Obsidian
-    ".obsidian/"
-    ".trash/"
   ];
 }
