41bd2e00990b76d12cce8a819b42f78824c86948
Sam A. Horvath-Hunt <hello@samhh.com>
2024-12-21 13:56:13 +0000

    Explicitly pass -g
    As recommended by fish's docs.

--- home/shell.nix
+++ home/shell.nix
@@ -34,7 +34,7 @@ in
     ];
 
     shellInit = ''
-      set fish_greeting
+      set -g fish_greeting
 
       if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
         fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
