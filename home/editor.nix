95789181fd09b4a1058ffdd1ea795619c86ce261
Sam A. Horvath-Hunt <hello@samhh.com>
2024-12-27 14:48:01 +0000

    Remove most of Helix config
    Zed is my primary editor at the moment, leaving Helix only for ephemeral stuff.

--- home/editor.nix
+++ home/editor.nix
@@ -8,40 +8,7 @@
 {
   programs.helix = {
     enable = true;
-    settings = {
-      editor = {
-        soft-wrap.enable = true;
-        rulers = [ 80 ];
-      };
-
-      keys =
-        let
-          movement = {
-            "{" = "goto_prev_paragraph";
-            "}" = "goto_next_paragraph";
-          };
-        in
-        {
-          normal = movement // {
-            # https://github.com/helix-editor/helix/issues/3001
-            c = "change_selection_noyank";
-            "A-c" = "change_selection";
-            d = "delete_selection_noyank";
-            "A-d" = "delete_selection";
-
-            space = {
-              l = "file_picker_in_current_buffer_directory";
-              q = ":buffer-close";
-              Q = ":buffer-close-all";
-              z = ":format";
-            };
-          };
-
-          select = movement // {
-            space.q = ":reflow";
-          };
-        };
-    };
+    settings.editor.rulers = [ 80 ];
   };
 
   programs.zed-editor = {
