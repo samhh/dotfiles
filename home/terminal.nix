3dd1f2c5d5fd6060850a69754faf696d337d099e
Sam A. Horvath-Hunt <hello@samhh.com>
2024-12-27 11:13:42 +0000

    Configure Ghostty quick terminal

--- home/terminal.nix
+++ home/terminal.nix
@@ -6,6 +6,9 @@
     background-opacity = 0.92
     cursor-style-blink = false
     font-size = 12
+    keybind = global:super+enter=toggle_quick_terminal
+    quick-terminal-animation-duration = 0
+    quick-terminal-position = center
     shell-integration-features = no-cursor
     theme = light:catppuccin-latte,dark:catppuccin-mocha
     window-padding-x = 8
