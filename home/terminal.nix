b6c754e2e3ac89b340e54e3327dcb3970fc82fea
Sam A. Horvath-Hunt <hello@samhh.com>
2024-12-27 10:26:53 +0000

    Kitty -> Ghostty
    There are PRs in both nixpkgs and home-manager that haven't merged yet.

--- home/terminal.nix
+++ home/terminal.nix
@@ -1,26 +1,13 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
-  programs.kitty = {
-    enable = true;
-    font = {
-      package = pkgs.hasklig;
-      name = "Hasklig Regular";
-    };
-    shellIntegration.mode = "no-cursor";
-    settings = {
-      remember_window_size = false;
-      enabled_layouts = "fat, grid";
-      visual_window_select_characters = "tnseriao";
-      scrollback_pager_history_size = "10";
-    };
-    keybindings = {
-      "cmd+w" = "discard_event";
-      "cmd+t" = "launch --cwd=current";
-      "cmd+l" = "next_layout";
-      "cmd+e" = "focus_visible_window";
-      "cmd+f" = "show_scrollback";
-      "cmd+p" = "show_last_command_output";
-    };
-  };
+  xdg.configFile."ghostty/config".text = ''
+    background-blur-radius = 20
+    background-opacity = 0.92
+    cursor-style-blink = false
+    font-size = 12
+    shell-integration-features = no-cursor
+    theme = light:catppuccin-latte,dark:catppuccin-mocha
+    window-padding-x = 8
+  '';
 }
