cf70fabbf61a2060419605623128980c62a56e68
Sam A. Horvath-Hunt <hello@samhh.com>
2024-12-29 19:49:17 +0000

    Change quick terminal keybind
    cmd+enter conflicts with Zed. cmd+` by contrast merely overrides switching
    between windows in macOS, which I'm never likely to use, and harmonises with
    selecting the terminal in Zed (ctrl+`).

--- home/terminal.nix
+++ home/terminal.nix
@@ -8,7 +8,7 @@
     background-opacity = 0.92
     cursor-style-blink = false
     font-size = 12
-    keybind = global:super+enter=toggle_quick_terminal
+    keybind = global:super+`=toggle_quick_terminal
     quick-terminal-animation-duration = 0
     quick-terminal-position = center
     shell-integration-features = no-cursor
